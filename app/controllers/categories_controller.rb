9ef378cea213a1f4e47e367ee3bd7d1f3b785216
Rafael Freire <rafaelcfreire@gmail.com>
2015-08-10 21:12:08 -0300

    First commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,14 @@
+class CategoriesController < ApplicationController
+	def index
+		@categories = Category.all
+	end
+
+	def show
+		@category = Category.find(params[:id])
+
+		respond_to do |format|
+			format.html
+			format.json { render json: @category }
+		end
+	end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoupToBits::Application.config.session_store :cookie_store, key: '_soup_to_bits_session'
